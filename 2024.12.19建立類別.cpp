49414028b2b66ac3b3c217ea2de8ac878ffaa916
billly980403 <313042@s.lhvs.tyc.edu.tw>
2024-12-19 15:24:05 +0800

    Update 2024.12.19建立類別.cpp

--- 2024.12.19建立類別.cpp
+++ 2024.12.19建立類別.cpp
@@ -1,14 +1,26 @@
-#include<iosream>
-using namesoace std;
+#include<iostream>
+using namespace std;
 
 class myTime{
     public:
         int hour;
-        int minute;
+        int minutes;
         void printTime();
-        
 };
 
+void myTime::printTime(){
+    cout<<hour<<":"<<minutes<<"\n";
+}
 int main(){
+    myTime now, open;
+    now.hour = 8;
+    now.minutes = 18;
+    cout<<"現在時間:";
+    now.printTime();
+    open.hour = 9;
+    open.minutes = 10;
+    cout<<"開門時間:";
+    open.printTime();
+    
     
 }
